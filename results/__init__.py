20834ce2787f8d10b0f664de75611b3b81aef21a
Courtney Pitcher <cr.pitcher@gmail.com>
2019-12-20 11:16:35 +0200

    Added docs

--- results/__init__.py
+++ results/__init__.py
@@ -2,5 +2,11 @@ import os
 
 
 def absolute_path(file_name: str) -> str:
+    """
+    Gets a file path in the results folder, regardless of the working directory.
+    i.e. 'iol.json' -> '/projects/project/results/iol.json'
+    :param file_name: A file name in the results folder.
+    :return: The absolute path.
+    """
     dir = os.path.dirname(os.path.abspath(__file__))
     return os.path.join(dir, file_name)
